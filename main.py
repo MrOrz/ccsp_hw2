6d246a66eca6b591f2b6489cdb2e6476e5027ece
MrOrz <johnsonliang7@gmail.com>
2011-05-05 15:08:32 +0800

    department id

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 #!/usr/bin/env python
 #
 # Copyright 2007 Google Inc.
@@ -18,38 +20,75 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.api.urlfetch import fetch
 from django.utils import simplejson as json
-import re # regex
+import re, cgi # regex, html escaping
 
 HOSPITAL = 'cgmh'
 
 # urls used in this app
 urls = {
     'prefix': 'http://www.cgmh.org.tw/register/',
-    'dept'  : 'rms_shk.htm'
+    'dept'  : 'rms_shk.htm',                    # dept listing
+    'dept_id' : 'RMSTimeTable.aspx?dpt={id}'    # dept info, given dept_id
 }
 
-def url_for(u):
+def url_for(u, args = {}):
     ### handles the url stuff ###
-    return urls['prefix'] + urls[u]
+    if args == {}:
+        return urls['prefix'] + urls[u]
+    else:
+        return urls['prefix'] + urls[u].format(**args)
 
-def u(big5_str):
+def u(encoded_str, dec = 'big5'):
     ### decode big-5 html from the hospital & convert to unicode string ###
-    return unicode(big5_str.decode('big5'))
+    return unicode(encoded_str.decode(dec))
 
 class DeptHandler(webapp.RequestHandler):
+
     ### department information & listing ###
     def get(self):
-        response = {}
         if self.request.get('id') == '':    # get all department
             html = u( fetch(url_for('dept')).content )
 
             # use regex to extract links directly. no soup needed :P
             lst = re.findall(r'<a href="RMSTimeTable\.aspx\?dpt=(\w+)">(.+)</a>', html)
             response = [{l[0]:l[1]} for l in lst]
-#            for l in lst:
-#                self.response.out.write(l[0] + ': ' + l[1] + '<br />')
             self.response.out.write(json.dumps(response))
+
         else:                               # get a specific department
+            dept_id = self.request.get('id')
+            html = u( fetch(url_for('dept_id', {'id':dept_id}), deadline=10).content, 'utf-8' )
+
+            # <a href="Login.aspx?rmsData=20110506PM581700N6351%e9%8d%be%e6%96%87%e6%a6%ae&amp;dptName=NNNN%e5%bf%83%e8%87%9f%e8%a1%80%e7%ae%a1%e5%85%a7%e7%a7%91&amp;dpt=81700A">26351鍾文榮</a>
+            regex = ''.join([
+                '<a href="Login.aspx\?rmsData=',
+                '(\d{4})(\d{2})(\d{2})(\w{2})', # time information
+                '\d.*',                         # don't know
+                'N(\d{4})',                     # doctor ID
+                '.*">\d+',                      # don't care
+                '([^<]*)',                      # doctor name (Chinese)
+                '</a>'
+            ])
+            lst  = re.findall(regex , html, re.UNICODE)
+            # FIXME: 只能找到每行第一個 match，所以幾乎只有早上的門診。
+
+            # >高雄長庚 一般婦產科</span>
+            dept_name = re.search(u'>高雄長庚 ([^<]*)', html).group(1)
+            doctor=[]
+            time=[]
+            for l in lst:
+                year, month, day, noon, doc_id, doc_name = l
+                if {doc_id: doc_name} not in doctor:
+                    doctor.append({doc_id: doc_name})
+
+                noon = {'AM':'A', 'PM':'B', 'NT':'C'}[noon]
+                time.append("%s-%s-%s-%s" % (year, month, day, noon))
+
+            response = [
+                {'id': dept_id},
+                {'name':dept_name},
+                {'doctor': doctor},
+                {'time': time}
+            ]
             self.response.out.write(json.dumps(response))
 
 class DoctorHandler(webapp.RequestHandler):
